bc1b2eb30ec2110241641e81faeb499e841d060a
Jay Nguyen <cuongnguyen29112@gmail.com>
2021-07-24 11:46:35 +1000

    Update setup.sh file

--- setup.sh
+++ setup.sh
@@ -13,5 +13,5 @@ fi
 
 /usr/bin/cp revshells ${HOMEDIR} && /usr/bin/chmod 755 "${HOMEDIR}/revshells"
 /usr/bin/cp rmenum ${HOMEDIR} && /usr/bin/chmod 755 "${HOMEDIR}/rmenum"
-echo -e "\n[+] You can execute: revshells commands to print all reverse shells."
+echo -e "\n[+] You can execute: \n\t+ revshells command to print all reverse shells commands.\n\t+ rmenum command to print all remote enumeration commands."
 echo -e "[+] Done\n"
